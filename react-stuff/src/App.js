2b5ebd1504e762ab8990eb2e23bdb13e276c7468
mugen13le <mugen4college@gmail.com>
2020-05-13 13:38:44 -0700

    eh

--- react-stuff/src/App.js
+++ react-stuff/src/App.js
@@ -1,29 +1,29 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
-import Error from './components/Error';
-import Navigation from './components/Navigation';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
+import Navigation from "./components/Navigation";
 import List1 from "./components/ListHeader";
- 
+
 class App extends Component {
   render() {
-    return (      
-       <BrowserRouter>
+    return (
+      <BrowserRouter>
         <div>
           <Navigation />
-            <Switch>
-             <Route path="/" component={Home} exact/>
-             <Route path="/about" component={About}/>
-             <Route path="/contact" component={Contact}/>
-             <Route path="/list/1" component={List1}/>
-            <Route component={Error}/>
-           </Switch>
-        </div> 
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/list/1" component={List1} />
+            <Route component={Error} />
+          </Switch>
+        </div>
       </BrowserRouter>
     );
   }
 }
- 
+
 export default App;
--- react-stuff/src/components/TodoList.js
+++ react-stuff/src/components/TodoList.js
@@ -1,71 +1,87 @@
 import React, { Component } from "react";
 import TodoItems from "./TodoItems";
 import "./TodoList.css";
- 
-class TodoList extends Component {
+import axios from "axios";
 
+class TodoList extends Component {
   constructor(props) {
     super(props);
- 
+
     this.state = {
-      items: []
+      items: [],
     };
 
     this.addItem = this.addItem.bind(this);
     this.deleteItem = this.deleteItem.bind(this);
   }
-
   addItem(e) {
     if (this._inputElement.value !== "") {
-    var newItem = {
-      text: this._inputElement.value,
-      key: Date.now()
-    };
- 
-    this.setState((prevState) => {
-      return { 
-        items: prevState.items.concat(newItem) 
+      var newItem = {
+        text: this._inputElement.value,
+        key: Date.now(),
       };
-    });
-   
-    this._inputElement.value = "";
+      this.makePostCall(newItem).then((callResult) => {
+        this.setState((prevState) => {
+          return {
+            items: prevState.items.concat(newItem),
+          };
+        });
+        this._inputElement.value = "";
+      });
+      console.log(this.state.items);
+      e.preventDefault();
+    }
   }
-   
-  console.log(this.state.items);
-     
-  e.preventDefault();
-
-
+  makePostCall(item) {
+    return axios
+      .post("http://localhost:5000/list/1", item)
+      .then(function (response) {
+        console.log(response);
+        return response;
+      })
+      .catch(function (error) {
+        console.log(error);
+        return false;
+      });
   }
 
   deleteItem(key) {
     var filteredItems = this.state.items.filter(function (item) {
-      return (item.key !== key);
+      return item.key !== key;
     });
-
     this.setState({
-      items: filteredItems
+      items: filteredItems,
     });
-
   }
-
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/list/1")
+      .then((res) => {
+        const items = res.data.users_list;
+        this.setState({ items });
+      })
+      .catch(function (error) {
+        //Not handling the error. Just logging into the console.
+        console.log(error);
+      });
+  }
 
   render() {
     return (
       <div className="todoListMain">
-        <div className="header">    
+        <div className="header">
           <form onSubmit={this.addItem}>
-              <input ref={(a) => this._inputElement = a} 
-                      placeholder="enter task">
-            </input>
+            <input
+              ref={(a) => (this._inputElement = a)}
+              placeholder="enter task"
+            ></input>
             <button type="submit">Add</button>
           </form>
         </div>
-        <TodoItems entries={this.state.items}
-                   delete={this.deleteItem}/>
+        <TodoItems entries={this.state.items} delete={this.deleteItem} />
       </div>
     );
   }
 }
- 
+
 export default TodoList;
